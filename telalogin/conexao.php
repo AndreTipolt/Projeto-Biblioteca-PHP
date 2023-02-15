01e1ae87e59d3865346a7c897404fb6c57fcacc4
André Tipolt <74414937+AndreTipolt@users.noreply.github.com>
2023-02-15 20:58:56 -0300

    Delete conexao.php

--- telalogin/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $server = "localhost:3306";
-    $user = "root";
-    $password = "Batata03";
-    $dbname = "bdBiblioteca";
-    $conn = mysqli_connect($server, $user, $password, $dbname);
-?>
